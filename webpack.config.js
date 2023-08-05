0c0a230b70fb112a2337c90604745d515c7899d9
m3n0l1f3 <ma4thx@gmail.com>
2023-08-05 17:18:33 +0800

    Reduce plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,18 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 
 const isProduction = process.env.NODE_ENV == 'production'
 
-const stylesHandler = isProduction
-    ? MiniCssExtractPlugin.loader
-    : 'style-loader'
-
 const config = {
+    mode: isProduction ? 'production' : 'development',
     entry: path.resolve(__dirname, 'src', 'index.ts'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-    },
+    output: { path: path.resolve(__dirname, 'dist') },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-        }),
         new CopyPlugin({
             patterns: [
                 {
                     from: '**/*.html',
-                    to: '[path][name].[ext]',
+                    to: '[path][name][ext]',
                     context: 'src',
                 },
             ],
@@ -42,7 +30,7 @@ const config = {
             },
             {
                 test: /\.css$/i,
-                use: [stylesHandler, 'css-loader'],
+                use: ['style-loader', 'css-loader'],
             },
             {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
@@ -58,13 +46,4 @@ const config = {
     },
 }
 
-module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production'
-
-        config.plugins.push(new MiniCssExtractPlugin())
-    } else {
-        config.mode = 'development'
-    }
-    return config
-}
+module.exports = config
