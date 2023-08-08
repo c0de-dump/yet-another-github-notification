c38eca03b1fce0ae6a056d84443672ed3d8e29f5
m3n0l1f3 <ma4thx@gmail.com>
2023-08-08 23:19:41 +0800

    update setting

--- src/setting.js
+++ src/setting.js
@@ -1,13 +1,20 @@
 function saveOptions(e) {
     console.log('saveOptions')
-    e.preventDefault()
     const form = new FormData(e.target)
     browser.storage.sync.set(Object.fromEntries(form.entries()))
     const node = document.querySelector('#id')
-    if (node == null) return
     node.textContent = JSON.stringify(form.entries())
     console.log(form.entries())
+    e.preventDefault()
 }
 
-console.log('loaded setting script')
-document.querySelector('form')?.addEventListener('submit', saveOptions)
+function restoreOptions() {
+    let storageItem = browser.storage.sync.get('token')
+    storageItem.then((res) => {
+        document.querySelector('#redirect-url').innerText = res.token
+    })
+}
+
+console.log(document)
+document.addEventListener('DOMContentLoaded', restoreOptions)
+document.querySelector('form').addEventListener('submit', saveOptions)
