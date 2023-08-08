2981b33d5c4374a984ada8eb7b5151d6fc43aec2
m3n0l1f3 <ma4thx@gmail.com>
2023-08-08 22:58:37 +0800

    remove tsg

--- src/background/index.ts
+++
@@ -1,9 +0,0 @@
-import browser from 'webextension-polyfill'
-
-async function Poll() {
-    console.log('Polling...')
-    const token = await browser.storage.sync.get('token')
-    console.log('token:', token)
-}
-
-browser.runtime.onInstalled.addListener(Poll)
