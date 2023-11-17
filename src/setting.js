9b81d81b6d6915e242ddb8b9fc7c65afb29617be
m3n0l1f3 <ma4thx@gmail.com>
2023-11-17 21:36:56 +0900

    Settings only

--- src/setting.js
+++ src/setting.js
@@ -1,10 +1,14 @@
+// var browser = require('webextension-polyfill')
+import browser from 'webextension-polyfill'
 // Description: setting page
 document.addEventListener('DOMContentLoaded', async () => {
     const form = document.querySelector('#setting-form')
 
     for (const elem of form?.querySelectorAll('[name]')) {
         data = await browser.storage.sync.get(elem.id)
-        elem.value = data[elem.id]
+        if (data[elem.id] !== undefined) {
+            elem.value = data[elem.id]
+        }
         elem.addEventListener('change', async (evt) => {
             browser.storage.sync.set({ [elem.id]: evt.target.value })
         })
