ca1f5f6bd5f371dd43bf218456d2abf972a46eea
m3n0l1f3 <ma4thx@gmail.com>
2023-11-25 02:14:35 +0900

    Working version

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,29 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 const isProduction = process.env.NODE_ENV == 'production'
 
 const config = {
-    entry: './src/index.js',
+    entry: {
+        background: path.resolve(__dirname, 'src/background.ts'),
+        setting: path.resolve(__dirname, 'src/setting.ts'),
+    },
+    devtool: 'source-map',
     output: {
         path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        open: true,
-        host: 'localhost',
+        filename: '[name].js',
+        clean: true,
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
+        new CopyPlugin({
+            patterns: [
+                {
+                    context: path.resolve(__dirname, 'src'),
+                    from: '**/*.html',
+                    to: path.resolve(__dirname, 'dist'),
+                },
+            ],
         }),
-
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
     ],
     module: {
         rules: [
@@ -33,10 +35,23 @@ const config = {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
                 type: 'asset',
             },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
+            {
+                test: /.(ts|tsx)?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-typescript'],
+                    },
+                },
+            },
         ],
+    },
+    resolve: {
+        extensions: ['.js', '.ts'],
+        alias: {
+            'webextension-polyfill-ts': path.resolve(__dirname, 'node_modules', 'webextension-polyfill-ts'),
+        },
     },
 }
 
