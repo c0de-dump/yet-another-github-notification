649ea7a78d272fa7af438586416a51ec8fa0c575
m3n0l1f3 <ma4thx@gmail.com>
2024-01-07 16:33:36 +0900

    lock npm version

--- src/background.ts
+++ src/background.ts
@@ -1,8 +1,9 @@
 import browser from 'webextension-polyfill'
 import type { Notification } from '@/types/github'
-import { ListNotifications } from './github'
+import { listNotifications } from './github'
 
 const second = 1000
+const _interval = 10 * second
 let timer: NodeJS.Timer
 
 async function _createNotification(notification: Notification) {
@@ -16,7 +17,13 @@ async function _createNotification(notification: Notification) {
 }
 
 async function _storeNotification(notification: Notification) {
-    // await browser.storage.local.set({ [notification.id]: notification })
+    await browser.storage.local.set({ [notification.id]: notification })
+}
+
+async function _newNotification(notification: Notification): Promise<boolean> {
+    const notifs = await browser.storage.local.get(notification.id)
+    const notif = notifs[notification.id]
+    return notif === undefined || notif.updated_at !== notification.updated_at
 }
 
 async function _checkNotifications() {
@@ -26,10 +33,15 @@ async function _checkNotifications() {
         console.debug('empty token from storage')
         return
     }
-    const notifications = await ListNotifications(token)
+    const notifications = await listNotifications(token)
+    // notifications is not an array when token is invalid
+    if (!Array.isArray(notifications)) return
     for (const notification of notifications) {
-        await _createNotification(notification)
-        await _storeNotification(notification)
+        console.log('check notification: ', notification)
+        if (await _newNotification(notification)) {
+            await _createNotification(notification)
+            await _storeNotification(notification)
+        }
     }
 }
 
@@ -41,15 +53,11 @@ browser.notifications.onClicked.addListener(async (id) => {
 browser.runtime.onStartup.addListener(() => {
     console.log('runtime started')
     clearInterval(timer)
-    timer = setInterval(() => {
-        _checkNotifications()
-    }, 10 * second)
+    timer = setInterval(_checkNotifications, _interval)
 })
 
 browser.runtime.onInstalled.addListener((details) => {
     console.log('runtime installed: ', JSON.stringify(details, null, 2))
     clearInterval(timer)
-    timer = setInterval(() => {
-        _checkNotifications()
-    }, 10 * second)
+    timer = setInterval(_checkNotifications, _interval)
 })
